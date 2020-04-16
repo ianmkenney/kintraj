da77ae9f118f9553e2eda0958c32d1fba7f9504b
Ian Kenney <ikenney@asu.edu>
2020-04-15 23:56:34 -0700

    poor implementation of command line args, will need rework

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use kintraj;
 use gnuplot::{Figure, Caption, Color};
+use clap::clap_app;
 
 fn main() {
-	let total_time: f64 = 100.0;
+
+    let matches = clap_app!(myapp => 
+        (@arg PLOT: -p --plot "Set to plot the signal")
+        ).get_matches();
+	let total_time: f64 = 1.0;
 	let dt: f64 = 1e-5;
 	let n: u64 = (total_time / dt) as u64;
 
@@ -10,7 +15,7 @@ fn main() {
 	println!("Simulation time of {} with timestep of {}", total_time, dt);
 
 	let mut res = kintraj::Timeseries::new();
-	let sg = kintraj::SignalGenerator::new(0.0004, 0.0009, dt);
+	let sg = kintraj::SignalGenerator::new(0.3, 0.01, dt);
 
     println!("Expected free energy difference is {:.7} kT", sg.delta_f());
     
@@ -21,8 +26,13 @@ fn main() {
 
     println!("Simulated free energy difference was {:.7} kT", res.delta_f());
 
-    let mut figure = Figure::new();
-    figure.axes2d()
-    .lines(res.get_time(), res.get_state(), &[Color("black")]);
-    figure.show();
+    match matches.occurrences_of("PLOT") {
+        0 => {},
+        _ => {
+            let mut figure = Figure::new();
+            figure.axes2d()
+            .lines(res.get_time(), res.get_state(), &[Color("black")]);
+            figure.show(); 
+        }
+    } 
 }
